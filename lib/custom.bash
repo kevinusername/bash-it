ee5b41ab141e05df72c8497ec627b546be4db438
Kevin Peters <35283725+kevinusername@users.noreply.github.com>
2020-03-26 19:54:59 -0700

    Add initial custom files

---
+++ lib/custom.bash
@@ -0,0 +1,15 @@
+export EDITOR=/usr/bin/nvim
+
+export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd -t d"
+
+export PATH="$HOME/.poetry/bin:$PATH"
+
+# if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+#     GIT_PROMPT_ONLY_IN_REPO=1
+#     GIT_PROMPT_THEME=Default_NoExitState
+#     source $HOME/.bash-git-prompt/gitprompt.sh
+# fi
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
